8343fadd572b1829990345ef1d70fcf271a8a4c3
Dominika Świeboda <dominika_swieboda@outlook.com>
2020-03-07 17:29:36 +0100

    corrections

--- app/js/script.js
+++ app/js/script.js
@@ -12,11 +12,32 @@ if (window.NodeList && !NodeList.prototype.forEach) {
 }
 
 $(document).ready(function(){
-  //Tooltip info
+  // Tooltip info
   $('[data-toggle="tooltip"]').tooltip();
-
-  //Datepicker
-  $('#datepicker').datepicker({
-    uiLibrary: 'bootstrap4',
+  // Datapicker
+  $('[data-toggle="datepicker"]').datepicker({
+    language: 'pl-PL'
   });
+  // Save document
+  $(' .multistep-form__action--save .btn-save').on('click', function(e){
+    e.preventDefault();
+    var btnSave = $(this);
+    var changeText = $('.change-text');
+    var changeIcon = $('.icon-save');
+    $(this).toggleClass('sending');
+    changeText.text('zapisywanie');
+    changeIcon.toggleClass('hidden');
+
+    setTimeout(function(){
+      btnSave.removeClass('sending');
+      changeText.text('zapisano');
+      btnSave.addClass('success');
+      setTimeout(function () {
+        btnSave.removeClass('success');
+        changeText.text('zapisz i dokończ później');
+        changeIcon.removeClass('hidden');
+      }, 1000);
+    },3000);
+
+  })
 });
